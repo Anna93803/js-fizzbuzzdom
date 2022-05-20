1fbc5c4c3e50ddfb8d9be8fbdbd156898191251a
Anna93803 <anna.tassone93@gmail.com>
2022-05-20 16:30:12 +0200

    add milestone 2

--- js/script.js
+++ js/script.js
@@ -5,27 +5,36 @@ for( let i = 1; i <= 100; i++) {
     // MILESTONE 1
     // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
     // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-    let multiploTre = "Fizz";
-    let multiploCinque = "Buzz";
-    let multiplo = multiploTre + multiploCinque;
+    let testoVisibile = i;
+    // let multiploTre = "Fizz";
+    // let multiploCinque = "Buzz";
+    // let multiplo = multiploTre + multiploCinque;
     
-
     // console.log(multiploTre, multiploCinque , multiplo);
-    console.log(multiplo);
-
+    
     if(i % 3 === 0 && i % 5 === 0) {
 
-        console.log(multiplo);
+        testoVisibile = "FizzBuzz";
+        // console.log(testoVisibile);
 
     }else if(i % 3 === 0) {
 
-        console.log(multiploTre);
+        testoVisibile = "Fizz";
+        // console.log(testoVisibile);
 
     }else if(i % 5 === 0) {
 
-        console.log(multiploCinque);
+        testoVisibile = "Buzz";
+        // console.log(testoVisibile);
     }
+
+    // MILESTONE 2
+    // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+
+    const squareDaStampare = document.getElementById("square");
+    squareDaStampare.innerHTML += `<div class="square">${testoVisibile}</div>`;
+    console.log(squareDaStampare);
+    
 }
 
 
